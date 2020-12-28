677a34b2f779bbec8b5abf4390c1f73943e12071
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2020-12-28 00:45:36 -0300

    Update v1.0
    Funcional, mas ainda necessita de cygwin
    Alterado install pra apagar lista

--- Install.bat
+++ Install.bat
@@ -1,19 +1,22 @@
 @echo off
 :: Diretório base é a pasta onde o script está
 set BASE_DIR=%~dp0
-set SCRIPT_PATH=%BASE_DIR%scripts\_Backup.bat
+set SCRIPT_BU_PATH=%BASE_DIR%scripts\_Backup.bat
 set SCRIPT_ABU_PATH=%BASE_DIR%scripts\abu_task.bat
+set LIST_PATH=%BASE_DIR%listas\LISTA_BACKUP.txt
 
 echo ----------------By TuLiO-------------------
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Instalando Auto-backup...
+del %LIST_PATH%
+copy NUL %LIST_PATH%
 echo -Inserindo atalho _Backup.bat no menu contexto "Enviar para"...
 ::Insere menu contexto "Enviar para"
-SET COMMAND="(Get-Content %SCRIPT_PATH%) | %%{$_ -replace 'BASE_DIR=\".*\"','BASE_DIR=\"%BASE_DIR%"'} | Set-Content -Path %SCRIPT_PATH%"
+SET COMMAND="(Get-Content %SCRIPT_BU_PATH%) | %%{$_ -replace 'BASE_DIR=\".*\"','BASE_DIR=\"%BASE_DIR%"'} | Set-Content -Path %SCRIPT_BU_PATH%"
 @PowerShell %COMMAND%
 SET COMMAND="(Get-Content %SCRIPT_ABU_PATH%) | %%{$_ -replace 'BASE_DIR=\".*\"','BASE_DIR=\"%BASE_DIR%"'} | Set-Content -Path %SCRIPT_ABU_PATH%"
 @PowerShell %COMMAND%
-::call C:\cygwin64\bin\bash.exe -l -c "sed 's/CD "".*""/CD "%SCRIPT_PATH%"/' ""%SCRIPT_PATH%"""
-xcopy "%SCRIPT_PATH%" "%APPDATA%\Microsoft\Windows\SendTo"
+::call C:\cygwin64\bin\bash.exe -l -c "sed 's/CD "".*""/CD "%SCRIPT_BU_PATH%"/' ""%SCRIPT_BU_PATH%"""
+xcopy "%SCRIPT_BU_PATH%" "%APPDATA%\Microsoft\Windows\SendTo"
 ::net user Administrador /active:yes
 echo -Agendando tarefa "Auto_backup"
 ::Cria tarefa no Task Scheduler
