c466985be3f384aca3e295a3f723680ed9b0b964
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-01-02 01:09:15 -0300

    Update 1.0

--- Install.bat
+++ Install.bat
@@ -1,7 +1,8 @@
 @echo off
 :: Diretório base é a pasta onde o script está
 set BASE_DIR=%~dp0
-set SCRIPT_BU_PATH=%BASE_DIR%scripts\_Backup.bat
+set SCRIPT_BU_PATH=%BASE_DIR%scripts\Backup.bat
+set SCRIPT_BU_TEMP_PATH=%BASE_DIR%\_Backup.bat
 set SCRIPT_ABU_PATH=%BASE_DIR%scripts\abu_task.bat
 set LIST_PATH=%BASE_DIR%listas\LISTA_BACKUP.txt
 
@@ -11,14 +12,12 @@ del %LIST_PATH%
 copy NUL %LIST_PATH%
 echo -Inserindo atalho _Backup.bat no menu contexto "Enviar para"...
 ::Insere menu contexto "Enviar para"
-SET COMMAND="(Get-Content %SCRIPT_BU_PATH%) | %%{$_ -replace 'BASE_DIR=.*','BASE_DIR=%BASE_DIR%'} | Set-Content -Path %SCRIPT_BU_PATH%"
-@PowerShell %COMMAND%
-SET COMMAND="(Get-Content %SCRIPT_ABU_PATH%) | %%{$_ -replace 'BASE_DIR=.*','BASE_DIR=%BASE_DIR%'} | Set-Content -Path %SCRIPT_ABU_PATH%"
-@PowerShell %COMMAND%
 ::call C:\cygwin64\bin\bash.exe -l -c "sed 's/CD "".*""/CD "%SCRIPT_BU_PATH%"/' ""%SCRIPT_BU_PATH%"""
-xcopy "%SCRIPT_BU_PATH%" "%APPDATA%\Microsoft\Windows\SendTo"
+SET COMMAND="(Get-Content %SCRIPT_BU_PATH%) | %%{$_ -replace 'BASE_DIR=.*','BASE_DIR=%BASE_DIR%'} | Set-Content -Path %SCRIPT_BU_TEMP_PATH%"
+@PowerShell %COMMAND%
+move "%SCRIPT_BU_TEMP_PATH%" "%APPDATA%\Microsoft\Windows\SendTo"
 ::net user Administrador /active:yes
-echo -Agendando tarefa "Auto_backup"
+echo -Agendanda tarefa "Auto_backup"
 ::Cria tarefa no Task Scheduler
 SCHTASKS /Create /RU SYSTEM /SC MINUTE /MO 5 /TN Auto_backup /TR "'%~dp0%scripts\abu_task.bat'"
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Instalado!
