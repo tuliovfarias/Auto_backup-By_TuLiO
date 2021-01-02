b2739340da96eedd30d189d0f4745755b2133a85
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-01-02 00:40:38 -0300

    Update abu_task.bat

--- scripts/abu_task.bat
+++ scripts/abu_task.bat
@@ -1,7 +1,7 @@
 :@echo off
 ::CD %~dp0
 ::CD ..
-SET BASE_DIR=C:\GitHub\Auto-backup\
+SET BASE_DIR=D:\GitHub\Auto-backup\
 
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Executando script...
 call C:\cygwin64\bin\bash.exe -l "%BASE_DIR%\scripts\auto_back-up.sh" > "%BASE_DIR%\log_abu.txt" 2>&1
