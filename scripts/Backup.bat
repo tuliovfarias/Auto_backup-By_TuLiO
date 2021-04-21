71d20a2ad5ed647c5ccdc8c096b8eaa959e2fc97
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-04-21 11:43:06 -0300

    Update Backup.bat

--- scripts/Backup.bat
+++ scripts/Backup.bat
@@ -9,6 +9,10 @@ cls
 set BASE_DIR=do not change
 set LISTA_BU_DIR=%BASE_DIR%\LISTA_BACKUP.txt
 set SCRIPT_ABU=%BASE_DIR%\scripts\auto_back-up.sh
+set LISTA_DRIVERS=%BASE_DIR%\drivers.txt
+
+if not exist %LISTA_DRIVERS% echo 1 >> %LISTA_DRIVERS%
+if not exist %LISTA_BU_DIR% type %LISTA_DRIVERS >> %LISTA_BU_DIR%
 
 echo [%date:~0,2%-%date:~3,2%-%date:~6,10% %time:~0,8%] Adicionar arquivo/pasta para lista de backup
 ::start shell:mycomputerfolder
