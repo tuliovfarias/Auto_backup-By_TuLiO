3eb852949202e49c90e4668de692530f76f22ca0
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-04-29 21:01:33 -0300

    Update auto_backup.py

--- scripts/auto_backup.py
+++ scripts/auto_backup.py
@@ -12,53 +12,70 @@ import pandas as pd
 import datetime
 from datetime import timedelta
 
-LAST_ID=os.path.join(os.path.dirname(__file__),'last_id.txt')
-BU_LIST = "\\bu_list.txt"
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
+LIST_DIR=os.path.join(BASE_DIR,'list')
+LAST_ID=os.path.join(LIST_DIR,'last_id.txt')
+DRIVER_ID = "\\driver_id.txt"
+BU_LIST_PATH=os.path.join(LIST_DIR,'backup_list.txt')
 
 print(time.strftime("%Y-%m-%d %H:%M:%S"),"Starting...")
 
 #sys.argv.pop(1) #remove segundo elemento da lista de argumentos (path pra este script)
 dest_bu=sys.argv[1]
-dest_letter=dest_bu.split('\\')[0]
 origin_dir_bu=os.path.dirname(sys.argv[2])
-origin_letter=origin_dir_bu.split('\\')[0]
 paths_bu=sys.argv[2:]
 
-bu_list_path_dest=os.path.join(dest_letter,BU_LIST)
-print('bu_list_path1:',bu_list_path_dest)
-bu_list_path_origin=os.path.join(origin_letter,BU_LIST)
-print('bu_list_path2:',bu_list_path_origin)
+dest_letter=dest_bu.split('\\')[0]
+origin_letter=origin_dir_bu.split('\\')[0]
+base_letter=BASE_DIR.split('\\')[0]
+
+if dest_letter==base_letter:
+    driver_id_dest_path=BU_LIST_PATH #para não dar erro de permissão
+else:
+    driver_id_dest_path=os.path.join(dest_letter,DRIVER_ID)
+if dest_letter==base_letter:  
+    driver_id_origin_path=BU_LIST_PATH #para não dar erro de permissão
+else:
+    driver_id_origin_path=os.path.join(origin_letter,DRIVER_ID)
+
+print('driver_id_dest_path:',driver_id_dest_path)
+print('driver_id_origin_path:',driver_id_origin_path)
 
 if not os.path.isfile(LAST_ID):
     with open(LAST_ID, 'w') as fid:
         fid.write("0") #caso primeiro back-up, escreve 0 na lista de ID
 
-with open(LAST_ID, 'r+') as fid:
-    if not os.path.isfile(bu_list_path_dest):
-        id=int(fid.read())+1 #soma 1 no id do driver
+if not os.path.isfile(driver_id_dest_path):
+    with open(LAST_ID, 'r+') as fid:
+        id_dest=int(fid.read())+1 #soma 1 no last_id (id do driver)
         fid.seek(0)
         fid.truncate() #apaga valor atual
-        fid.write(str(id)) #coloca o valor incrementado  
-        with open(bu_list_path_dest, 'w') as fd:
-            print('Driver-ID:',id, file=fd)        
-    if not os.path.isfile(bu_list_path_origin):
-        id=id+1 #soma 1 no id do driver
+        fid.write(str(id_dest)) #coloca o valor incrementado  
+    with open(driver_id_dest_path, 'w') as fd:
+        print(id_dest, file=fd)
+else: 
+    with open(driver_id_dest_path, 'r') as fd:
+        id_dest=int(fd.readline().strip()) #Caso já exista id configurado para o driver, apenas lê       
+if not os.path.isfile(driver_id_origin_path):
+    with open(LAST_ID, 'r+') as fid:
+        id_origin=int(fid.read())+1 #soma 1 no id do driver
         fid.seek(0)
         fid.truncate() #apaga valor atual
-        fid.write(str(id)) #coloca o valor incrementado  
-        with open(bu_list_path_origin, 'w') as fo:
-            print('Driver-ID:',id, file=fo)
+        fid.write(str(id_origin)) #coloca o valor incrementado  
+    with open(driver_id_origin_path, 'w') as fo:
+        print(id_origin, file=fo)
+else: 
+    with open(driver_id_origin_path, 'r') as fo:
+        id_origin=int(fo.readline().strip()) #Caso já exista id configurado para o driver, apenas lê
      
-with open(bu_list_path_dest, 'a') as fd, open(bu_list_path_origin, 'a') as fo:
+with open(BU_LIST_PATH, 'a') as f:
     line='      '
     for path_bu in paths_bu:
         if path_bu==paths_bu[-1]:
             line+=os.path.basename(path_bu)
         else:
             line+=os.path.basename(path_bu)+','
-    for f in [fd,fo]:
-        print('----->'+origin_dir_bu+'>'+dest_bu, file=f)
-        print(line, file=f) #f.write(line)
-        if dest_letter==origin_letter:
-            break #para evitar de escrever 2 vezes no mesmo arquivo
+    print('----->ORIGIN>'+id_origin+'>'+origin_dir_bu+'>DEST>'+id_dest+'>'+dest_bu, file=f)
+    print(line, file=f) #f.write(line)
 
+# %%
