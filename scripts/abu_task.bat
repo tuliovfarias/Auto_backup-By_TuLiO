1877b072df77a2ef669995f30f5c58ae0d318a4a
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-05-02 21:55:39 -0300

    Update abu_task.bat

--- scripts/abu_task.bat
+++ scripts/abu_task.bat
@@ -3,5 +3,7 @@
 SET BASE_DIR=%~dp0
 
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Executando script...
+:loop
 python "%BASE_DIR%\auto_backup.py" > "%BASE_DIR%\log_abu.txt" 2>&1
+goto loop
 ::pause
