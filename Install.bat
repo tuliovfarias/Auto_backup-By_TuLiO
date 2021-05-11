983829894cb712f304975a5461b5b10ed53efb36
Túlio Vieira <tuliovfarias@users.noreply.github.com>
2021-05-10 22:50:18 -0300

    Update stable

--- Install.bat
+++ Install.bat
@@ -4,12 +4,12 @@ set BASE_DIR=%~dp0
 set SCRIPT_BU_PATH=%BASE_DIR%scripts\Backup.bat
 set SCRIPT_BU_TEMP_PATH=%BASE_DIR%\_Backup.bat
 set SCRIPT_ABU_PATH=%BASE_DIR%scripts\abu_task.bat
-set LIST_PATH=%BASE_DIR%listas\LISTA_BACKUP.txt
+set LIST_PATH=%BASE_DIR%list\backup_list.txt
 
-echo ----------------By TuLiO-------------------
+echo ----------------Auto Backup By TuLiO-------------------
 echo [%date:~6,10%-%date:~3,2%-%date:~0,2% %time:~0,8%] Instalando Auto-backup...
-del %LIST_PATH%
-copy NUL %LIST_PATH%
+del %LIST_PATH% >nul 2>&1
+::copy NUL %LIST_PATH% >nul 2>&1
 echo -Inserindo atalho _Backup.bat no menu contexto "Enviar para"...
 ::Insere menu contexto "Enviar para"
 ::call C:\cygwin64\bin\bash.exe -l -c "sed 's/CD "".*""/CD "%SCRIPT_BU_PATH%"/' ""%SCRIPT_BU_PATH%"""
